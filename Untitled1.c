eb3b51145861205bbd7c42d0287314867e9c6977
Rubayad Hasan <gumbare77@gmail.com>
2024-10-06 21:48:03 +0600

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main()
+{
+
+    int tourday;
+    printf("enter your day");
+    scanf("%d",&tourday);
+
+    if(tourday==2){
+        bool isboatavailable = false;
+    if(isboatavailable)
+    {
+        printf("going to the houre\n");
+    }
+    else
+        {
+            printf("going to the shrimongol\n");
+        }
+    }
+    else if(tourday==4){
+        bool isshipavaible=0;
+        if("isshipavaible")
+        {
+            printf("going to the saint martin\n");
+        }
+        else
+        {
+            printf("going to the cox bazar\n");
+        }
+    }
+    else
+    {
+        printf("tour is cancel\n");
+
+    }
+    return  0;
+}
+
